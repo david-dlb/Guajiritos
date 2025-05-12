fcea4156f02bee1dd7b98b6364e0fbe0737700fc
david <david-dlb>
2025-05-12 14:07:45 -0400

    errores arreglados

--- front/src/app/app.routes.server.ts
+++ front/src/app/app.routes.server.ts
@@ -3,14 +3,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'dashboard/user-form/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'dashboard/task-form/:id',
-    renderMode: RenderMode.Client
-  }
+    renderMode: RenderMode.Server
+  }, 
 ];
